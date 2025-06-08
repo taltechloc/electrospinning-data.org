76241ff63de1e17f48c42b57c3a5c911e48c0ad4
mehrab <memahdian@outlook.com>
2025-06-08 14:09:47 +0300

    Renamed moderation endpoint

--- backend/my-backend/install/prod_build.sh
+++ backend/my-backend/install/prod_build.sh
@@ -3,4 +3,12 @@
 set -e
 cd ..
 
+export DB_URL=jdbc:mysql://localhost:3306/electrospinning
+export DB_USERNAME=root
+export APP_CORS_ALLOWED_ORIGIN="http://172.20.92.99"
+export DB_PASSWORD='Mysql@25!'
+export JWT_SECRET='YourVerySecretKey1234567890'
+export ADMIN_USERNAME='admin'
+export ADMIN_PASSWORD='admin'
+
 ./mvnw clean package
