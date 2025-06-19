97db0f366f6b3759dacd2b1e055c2f8d654ca4b1
mehrab <memahdian@outlook.com>
2025-06-19 13:40:56 +0300

    Data set page simplified

--- frontend/sw/src/pages/DatasetPage.js
+++ frontend/sw/src/pages/DatasetPage.js
@@ -53,8 +53,6 @@ const DatasetPage = () => {
             <h2>📁 Dataset Details</h2>
             <ul style={styles.datasetInfo}>
               <li style={styles.datasetInfoItem}>Format: Excel (.xlsx)</li>
-              <li style={styles.datasetInfoItem}>Size: ~30 KB</li>
-              <li style={styles.datasetInfoItem}>Last Updated: <span id="updated-date">June 20025</span></li>
             </ul>
           </section>
 
